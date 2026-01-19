520f55a4ff6f4ff767dd5d7cb67ecb7b4a99eab1
Vishwajit <shindevishwajit777@gmail.com>
2026-01-19 18:55:46 +0530

    uppercase

---
+++ uppercase.java
@@ -0,0 +1,7 @@
+public class uppercase{
+    public static void main(String args[]){
+        // Scanner sc=new Scanner(System.in);
+        char s='A';
+        System.out.println((int) s);
+    }
+}
