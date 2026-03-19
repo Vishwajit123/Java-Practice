28b8e8f01d6a227683ccf28d84329fffabd81478
Vishwajit <shindevishwajit777@gmail.com>
2026-03-20 00:31:46 +0530

    create unique pattern

---
+++ Pattern/Cuboid3D.java
@@ -0,0 +1,23 @@
+class Cuboid3D{
+    public static void main(String[] main){
+        int n =21;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                   if(  i==(n-1)/2 || j==(n-1)/2 || 
+                        i==0 && j>n/2 || i==n-1 && j < n/2 ||
+                        j==0 && i>n/2 || j==n-1 && i < n/2 ||
+                        i==n/4 && j>n/4 && j<3*n/4 || i==3*n/4 && j>n/4 && j<3*n/4 ||
+                        j==n/4 && i>=n/4 && i<=3*n/4 || j==3*n/4 && i>=n/4 && i<=3*n/4 ||
+                        i+j==n-1 || i+j==(n-1)/2 || i+j==3*n/2)
+                {
+                    System.out.print(" * ");
+                }
+                else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern/Swastiktwist.java
@@ -0,0 +1,21 @@
+class Swastiktwist{
+    public static void main(String[] main){
+        int n =11;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                   if(  i==(n-1)/2 || j==(n-1)/2 || 
+                        i==0 && j>n/2 || i==n-1 && j < n/2 ||
+                        j==0 && i>n/2 || j==n-1 && i < n/2 ||
+                        i==n/4 && j== 3*n/4 || i== 3*n/4 && j ==n/4 )
+                {
+                    System.out.print(" * ");
+                }
+                else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
